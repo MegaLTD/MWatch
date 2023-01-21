aa7439044ff8f64a1d5dc28ca153a3394570dcec
MegaLTD <102382969+MegaLTD@users.noreply.github.com>
2023-01-21 21:38:13 +0100

    Add files via upload

---
+++ رمضان-2023/Al-Kabeer-Awi-7/Ep1/script.js
@@ -0,0 +1,37 @@
+function RI(min, max){
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function ThisEp(){
+	let i = RI(0, 9);
+	const links = [
+		"https://75.cn.eu.org/MegaWatch/Al-Kabeer-Awi-7/E1/39qpKU2sQc.html", 
+		"https://cooralive.ml/MegaWatch/Al-Kabeer-Awi-7/E1/39qpKU2sQc.html", 
+		"https://psyplay.ga/MegaWatch/Al-Kabeer-Awi-7/E1/39qpKU2sQc.html",
+		"https://909.jp.eu.org/MegaWatch/Al-Kabeer-Awi-7/E1/39qpKU2sQc.html",
+		"https://81.fr.eu.org/MegaWatch/Al-Kabeer-Awi-7/E1/39qpKU2sQc.html",
+		"https://5005.eu.org/MegaWatch/Al-Kabeer-Awi-7/E1/39qpKU2sQc.html",
+		"https://123.asso.eu.org/MegaWatch/Al-Kabeer-Awi-7/E1/39qpKU2sQc.html",
+		"https://123.int.eu.org/MegaWatch/Al-Kabeer-Awi-7/E1/39qpKU2sQc.html",
+		"https://80.mk.eu.org/MegaWatch/Al-Kabeer-Awi-7/E1/39qpKU2sQc.html",
+		"https://99.ru.eu.org/MegaWatch/Al-Kabeer-Awi-7/E1/39qpKU2sQc.html"
+		];
+
+	window.location.href=links[i];
+}
+function NextEp(){
+	let j = RI(0, 9);
+	const links = [
+		"https://75.cn.eu.org/MegaWatch/Al-Kabeer-Awi-7/E2/4nJVQrVpvU.html", 
+		"https://cooralive.ml/MegaWatch/Al-Kabeer-Awi-7/E2/4nJVQrVpvU.html", 
+		"https://psyplay.ga/MegaWatch/Al-Kabeer-Awi-7/E2/4nJVQrVpvU.html",
+		"https://909.jp.eu.org/MegaWatch/Al-Kabeer-Awi-7/E2/4nJVQrVpvU.html",
+		"https://81.fr.eu.org/MegaWatch/Al-Kabeer-Awi-7/E2/4nJVQrVpvU.html",
+		"https://5005.eu.org/MegaWatch/Al-Kabeer-Awi-7/E2/4nJVQrVpvU.html",
+		"https://123.asso.eu.org/MegaWatch/Al-Kabeer-Awi-7/E2/4nJVQrVpvU.html",
+		"https://123.int.eu.org/MegaWatch/Al-Kabeer-Awi-7/E2/4nJVQrVpvU.html",
+		"https://80.mk.eu.org/MegaWatch/Al-Kabeer-Awi-7/E2/4nJVQrVpvU.html",
+		"https://99.ru.eu.org/MegaWatch/Al-Kabeer-Awi-7/E2/4nJVQrVpvU.html"
+		];
+
+	window.location.href=links[j];
+}
