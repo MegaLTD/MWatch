0fab7e7d7c217b0811b8ae55726c4f2890455bf9
MegaLTD <102382969+MegaLTD@users.noreply.github.com>
2023-03-19 22:09:49 +0100

    Update script.js

--- رمضان-2023/Al-Kabeer-Awi-7/Ep2/script.js
+++ رمضان-2023/Al-Kabeer-Awi-7/Ep2/script.js
@@ -1,13 +1,13 @@
 function RI(min, max){
-		return Math.floor(Math.random() * (max - min + 1)) + min;
+      return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 function ThisEp(){
-	let i = RI(0, 2);
-	const links = [
-		"https://909.jp.eu.org/MegaWatch/Al-Kabeer-Awi-7/E2/4nJVQrVpvU.html",
-		"https://5005.eu.org/MegaWatch/Al-Kabeer-Awi-7/E2/4nJVQrVpvU.html",
-		"https://99.ru.eu.org/MegaWatch/Al-Kabeer-Awi-7/E2/4nJVQrVpvU.html"
-		];
+   let i = RI(0, 2);
+   const links = [
+      "https://909.jp.eu.org/MegaWatch/Al-Kabeer-Awi-7/E2/4nJVQrVpvU.html",
+      "https://5005.eu.org/MegaWatch/Al-Kabeer-Awi-7/E2/4nJVQrVpvU.html",
+      "https://99.ru.eu.org/MegaWatch/Al-Kabeer-Awi-7/E2/4nJVQrVpvU.html"
+      ];
 
-	window.location.href=links[i];
+   window.location.href=links[i];
 }
