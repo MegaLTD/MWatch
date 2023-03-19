68e17aea59b93eeaf15881f3a2a3d8b63af15018
MegaLTD <102382969+MegaLTD@users.noreply.github.com>
2023-03-19 15:39:22 +0100

    Add files via upload

---
+++ رمضان-2023/Al-Kabeer-Awi-7/Ep2/script.js
@@ -0,0 +1,33 @@
+function RI(min, max){
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function ThisEp(){
+	let i = RI(0, 2);
+	const links = [
+		"https://909.jp.eu.org/MegaWatch/Al-Kabeer-Awi-7/E2/4nJVQrVpvU.html",
+		"https://5005.eu.org/MegaWatch/Al-Kabeer-Awi-7/E2/4nJVQrVpvU.html",
+		"https://99.ru.eu.org/MegaWatch/Al-Kabeer-Awi-7/E2/4nJVQrVpvU.html"
+		];
+
+	window.location.href=links[i];
+}
+function NextEp(){
+	let j = RI(0, 2);
+	const links = [
+		"https://909.jp.eu.org/MegaWatch/Al-Kabeer-Awi-7/E3/CBaiOKb3IP.html",
+		"https://5005.eu.org/MegaWatch/Al-Kabeer-Awi-7/E3/CBaiOKb3IP.html",
+		"https://99.ru.eu.org/MegaWatch/Al-Kabeer-Awi-7/E3/CBaiOKb3IP.html"
+		];
+
+	window.location.href=links[j];
+}
+function PrevEp(){
+	let z = RI(0, 2);
+	const links = [
+		"https://909.jp.eu.org/MegaWatch/Al-Kabeer-Awi-7/E1/39qpKU2sQc.html",
+		"https://5005.eu.org/MegaWatch/Al-Kabeer-Awi-7/E1/39qpKU2sQc.html",
+		"https://99.ru.eu.org/MegaWatch/Al-Kabeer-Awi-7/E1/39qpKU2sQc.html"
+		];
+
+	window.location.href=links[z];
+}
