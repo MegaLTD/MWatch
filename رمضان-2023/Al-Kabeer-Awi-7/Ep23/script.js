f1cc41d320668d8fa9d695230a9202ed689619e2
MegaLTD <102382969+MegaLTD@users.noreply.github.com>
2023-03-19 22:21:46 +0100

    Add files via upload

---
+++ رمضان-2023/Al-Kabeer-Awi-7/Ep23/script.js
@@ -0,0 +1,13 @@
+function RI(min, max){
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function ThisEp(){
+   let i = RI(0, 2);
+   const links = [
+      "https://909.jp.eu.org/MegaWatch/Al-Kabeer-Awi-7/E23/3NxAmrIW5p.html",
+         "https://5005.eu.org/MegaWatch/Al-Kabeer-Awi-7/E23/3NxAmrIW5p.html",
+            "https://99.ru.eu.org/MegaWatch/Al-Kabeer-Awi-7/E23/3NxAmrIW5p.html"
+   ];
+
+   window.location.href=links[i];
+}
