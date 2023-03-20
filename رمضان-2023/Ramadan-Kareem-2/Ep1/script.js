4083f38b02cb8a6ccc5ca4c2fc889d7054adc159
MegaLTD <102382969+MegaLTD@users.noreply.github.com>
2023-03-20 23:04:04 +0100

    Add files via upload

---
+++ رمضان-2023/Ramadan-Kareem-2/Ep1/script.js
@@ -0,0 +1,13 @@
+function RI(min, max){
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function ThisEp(){
+	let i = RI(0, 2);
+	const links = [
+		"https://909.jp.eu.org/MegaWatch/Ramadan-Kareem-2/E1/",
+		"https://5005.eu.org/MegaWatch/Ramadan-Kareem-2/E1/",
+		"https://99.ru.eu.org/MegaWatch/Ramadan-Kareem-2/E1/"
+	];
+
+	window.location.href=links[i];
+}
