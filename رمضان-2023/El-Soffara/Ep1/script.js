c70f2f0f9c3b1dec20b32f01a473ffc9e4bae87f
MegaLTD <102382969+MegaLTD@users.noreply.github.com>
2023-03-20 13:45:00 +0100

    Add files via upload

---
+++ رمضان-2023/El-Soffara/Ep1/script.js
@@ -0,0 +1,13 @@
+function RI(min, max){
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function ThisEp(){
+	let i = RI(0, 2);
+	const links = [
+		"https://909.jp.eu.org/MegaWatch/El-Soffara/E1/",
+		"https://5005.eu.org/MegaWatch/El-Soffara/E1/",
+		"https://99.ru.eu.org/MegaWatch/El-Soffara/E1/"
+	];
+
+	window.location.href=links[i];
+}
