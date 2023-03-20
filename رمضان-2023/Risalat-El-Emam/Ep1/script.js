13d6979783b9401db81cb82d5c848ab6148481ad
MegaLTD <102382969+MegaLTD@users.noreply.github.com>
2023-03-20 18:27:27 +0100

    Add files via upload

---
+++ رمضان-2023/Risalat-El-Emam/Ep1/script.js
@@ -0,0 +1,13 @@
+function RI(min, max){
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function ThisEp(){
+	let i = RI(0, 2);
+	const links = [
+		"https://909.jp.eu.org/MegaWatch/Risalat-El-Emam/E1/",
+		"https://5005.eu.org/MegaWatch/Risalat-El-Emam/E1/",
+		"https://99.ru.eu.org/MegaWatch/Risalat-El-Emam/E1/"
+	];
+
+	window.location.href=links[i];
+}
