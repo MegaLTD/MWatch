94fb1278693e3ba1b005093f07f7a6b29b3be095
MegaLTD <102382969+MegaLTD@users.noreply.github.com>
2023-05-27 10:47:39 +0100

    Add files via upload

---
+++ HTS/HTSscript.js
@@ -0,0 +1,13 @@
+function HTS(){
+	let H=["428910.42web.io",
+		"1111.us.eu.org",
+		"12.sk.eu.org",
+		"6006.rf.gd",
+		"000.au.eu.org",
+		"444.unaux.com",
+		"10001.unaux.com"];
+
+	let i=Math.floor(Math.random() * 7);
+
+	return H[i];
+}
