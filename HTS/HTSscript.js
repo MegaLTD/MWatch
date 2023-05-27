ae8dd04876b83ecd4e345de35062d0cd1d47bf94
MegaLTD <102382969+MegaLTD@users.noreply.github.com>
2023-05-27 10:58:48 +0100

    Update HTSscript.js

--- HTS/HTSscript.js
+++ HTS/HTSscript.js
@@ -1,4 +1,4 @@
-function HTS(){
+function HTS(id,ep){
 	let H=["428910.42web.io",
 		"1111.us.eu.org",
 		"12.sk.eu.org",
@@ -6,8 +6,8 @@ function HTS(){
 		"000.au.eu.org",
 		"444.unaux.com",
 		"10001.unaux.com"];
-
 	let i=Math.floor(Math.random() * 7);
+	let link = "http://"+H[i]+"/?id="+id+"&ep="+ep;
+	document.getElementById("HTSLink").setAttribute("href", link);
 
-	return H[i];
 }
