c160822a37a45959c3026e6dfae6312df39992ee
MegaLTD <102382969+MegaLTD@users.noreply.github.com>
2023-05-30 15:10:55 +0100

    Update HTSscript.js

--- HTS/HTSscript.js
+++ HTS/HTSscript.js
@@ -6,8 +6,9 @@ function HTS(id,ep){
 	       "8811.unaux.com", "6446.unaux.com",
 	       "5.liveblog365.com", "22.unaux.com",
 	      "0.liveblog365.com", "223344.unaux.com",
-	      "001.liveblog365.com"];
-	let i=Math.floor(Math.random() * 15);
+	      "001.liveblog365.com", "75.cn.eu.org",
+	      "81.fr.eu.org", "80.mk.eu.org"];
+	let i=Math.floor(Math.random() * 18);
 	let link = "http://"+H[i]+"/MegaWatch/?id="+id+"&ep="+ep;
 	document.getElementById("HTSLink").setAttribute("href", link);
 
