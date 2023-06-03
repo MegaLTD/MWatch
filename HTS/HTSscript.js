cfda559aa7a56d0b6a78a6115d2e001786d1334a
MegaLTD <102382969+MegaLTD@users.noreply.github.com>
2023-06-03 19:21:53 +0100

    Update HTSscript.js

--- HTS/HTSscript.js
+++ HTS/HTSscript.js
@@ -5,8 +5,7 @@ function HTS(id,ep){
 	       "koora-live.gq", "n-e-t-f-l-i-x.gq", "7.liveblog365.com",
 	       "8811.unaux.com", "6446.unaux.com", "555.000.pe",
 	       "5.liveblog365.com", "22.unaux.com", "90909.unaux.com",
-	      "0.liveblog365.com", "223344.unaux.com",
-	      "001.liveblog365.com"];
+	      "0.liveblog365.com", "223344.unaux.com", "001.liveblog365.com"];
 	let i=Math.floor(Math.random() * 21);
 	let link;
 	if(ep=="F"){
