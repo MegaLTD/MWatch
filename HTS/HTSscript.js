033d1e71ef668b6ad20c9a5217fb255c12e979a5
MegaLTD <102382969+MegaLTD@users.noreply.github.com>
2023-06-06 22:03:57 +0100

    Update HTSscript.js

--- HTS/HTSscript.js
+++ HTS/HTSscript.js
@@ -5,10 +5,10 @@ function HTS(id,ep){
 	       "koora-live.gq", "n-e-t-f-l-i-x.gq", "7.liveblog365.com", "66.000.pe",
 	       "6446.unaux.com", "555.000.pe", "4000.rf.gd", "55.42web.io",
 	       "5.liveblog365.com", "22.unaux.com", "90909.unaux.com", "77.rf.gd",
-	      "0.liveblog365.com", "223344.unaux.com", "001.liveblog365.com",
-	      "155.liveblog365.com", "301.unaux.com", "10101.unaux.com",
-	      "8177.000.pe", "2000.000.pe", "3210.rf.gd"];
-	let i=Math.floor(Math.random() * 33);
+	      "0.liveblog365.com", "223344.unaux.com", "001.liveblog365.com", "78.rf.gd",
+	      "155.liveblog365.com", "301.unaux.com", "10101.unaux.com", "9.42web.io",
+	      "8177.000.pe", "2000.000.pe", "3210.rf.gd", "15.000.pe"];
+	let i=Math.floor(Math.random() * 36);
 	let link;
 	if(ep=="F"){
 		link = "http://"+H[i]+"/MegaWatch/movie.php?id="+id;
