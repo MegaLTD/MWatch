f07cb1867185ec7ebd2f312a22f4435d4ee16ce1
MegaLTD <102382969+MegaLTD@users.noreply.github.com>
2024-11-17 20:51:28 +0100

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,61 @@
+fetch('series.json')
+    .then(response => response.json())
+    .then(data => {
+        const container = document.getElementById('grid-container');
+        
+        data.forEach(item => {
+            const gridItem = document.createElement('div');
+            gridItem.classList.add('grid-item');
+            gridItem.setAttribute(
+                'data-type', 
+                item.nbr_episodes === 0 || item.nbr_episodes === null ? 'film' : 'serie'
+            );
+
+            gridItem.innerHTML = `
+            <div class="card">
+                <a href="http://localhost/megawatch.eu.org/ramadan/?id=${item.id}">
+                    <img src="${item.image}" alt="${item.titre}" class="card-image">
+                </a>
+                <div class="card-content">
+                    <a href="http://localhost/megawatch.eu.org/ramadan/?id=${item.id}" class="card-title">
+                        <h3>${item.titre}</h3>
+                    </a>
+                    <p class="card-description">
+                        ${item.annee} - ${item.nbr_episodes === 0 || item.nbr_episodes === null ? 'Film' : 'Série'}
+                    </p>
+                    <center><a href="http://localhost/megawatch.eu.org/ramadan/?id=${item.id}" class="watch-button">شاهد الآن</a></center>
+                </div>
+            </div>
+        `;
+        
+
+            container.appendChild(gridItem);
+        });
+    })
+    .catch(error => console.error('Erreur lors du chargement des données:', error));
+
+function filterContent(type) {
+    const items = document.querySelectorAll('.grid-item');
+    items.forEach(item => {
+        if (type === 'all' || item.getAttribute('data-type') === type) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+}
+
+
+function searchContent(event) {
+    const query = event.target.value.toLowerCase(); 
+    const items = document.querySelectorAll('.grid-item');
+
+    items.forEach(item => {
+        const title = item.querySelector('h3').textContent.toLowerCase();
+        if (title.includes(query)) {
+            item.style.display = 'block'; 
+        } else {
+            item.style.display = 'none'; 
+        }
+    });
+}
