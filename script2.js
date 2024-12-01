a4e0c5566a9c5791456038df7a0a1be8e0bd65e4
MegaLTD <102382969+MegaLTD@users.noreply.github.com>
2024-12-01 20:39:24 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -43,6 +43,29 @@ if (!checkParamsAndRedirect()) {
             }
         })
         .catch(error => console.error("Erreur lors du chargement des séries:", error));
-
-
 }
+
+
+
+function generateRandomTime() {
+            // Générer une minute aléatoire entre 30 et 59
+            const minutes = Math.floor(Math.random() * 30) + 30;
+            // Générer une seconde aléatoire entre 0 et 59
+            const seconds = Math.floor(Math.random() * 60);
+            
+            // Formater les minutes et les secondes pour avoir toujours deux chiffres
+            const formattedMinutes = minutes.toString().padStart(2, '0');
+            const formattedSeconds = seconds.toString().padStart(2, '0');
+
+            // Retourner le temps formaté
+            return `${formattedMinutes}:${formattedSeconds}`;
+        }
+
+        // Mettre à jour le contenu de l'élément avec l'ID 'time-display'
+        function updateTime() {
+            const timeDisplay = document.getElementById('time-display');
+            timeDisplay.textContent = generateRandomTime();
+        }
+
+        // Mettre à jour le temps toutes les 3 secondes
+        setInterval(updateTime, 3000);
